9a73b653ce5e13fbbe2f9a4cd30b0404dc6f6c32
Toby-Codes <twiseas9ice@gmail.com>
2021-06-20 13:03:45 -0700

    Fixed script to taks 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main(void){
-  puts("\"Programming is like building a multilingual puzzle"
-       );
-  return (0);
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero if otherwise
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
