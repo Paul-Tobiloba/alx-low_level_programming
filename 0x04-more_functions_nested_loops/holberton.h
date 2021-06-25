3b0ec637b385b5ae928410aba34dd0d2b13e34e8
Toby-Codes <twiseas9ice@gmail.com>
2021-06-25 13:04:42 -0700

    Added holberton.h file

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,12 @@
+#ifndef FILE_HOLBERTON
+#define FILE_HOLBERTON
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+
+#endif
