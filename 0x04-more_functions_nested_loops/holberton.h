2c495fda973279021648ac6181093b069ebe8d3a
Toby-Codes <twiseas9ice@gmail.com>
2021-06-25 13:36:34 -0700

    Added solution to task 6 to 9

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,5 +1,5 @@
-#ifndef FILE_HOLBERTON
-#define FILE_HOLBERTON
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
 int _putchar(char c);
 int _isupper(int c);
@@ -8,5 +8,8 @@ int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
-
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
 #endif
