c8205e9c244cf5d526f9b4ceec4bcff8b1a711da
Toby-Codes <twiseas9ice@gmail.com>
2021-06-28 11:51:15 -0700

    Add: Solution to task 6 to 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * *_strcpy - copies the string pointed by src
+ * @dest: copy of the string
+ * @src: string to be copied
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
