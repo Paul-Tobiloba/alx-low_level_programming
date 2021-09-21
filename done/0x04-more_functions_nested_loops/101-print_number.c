9e7c6431b4879117fb8af5e63c465d2ccb35d182
Toby-Codes <twiseas9ice@gmail.com>
2021-09-20 19:45:57 -0700

    [Add] Solution to 0 and 1

---
+++ done/0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	} 
+
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
+}
---
+++ done/0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * followed by a new line
+ */
+void more_numbers(void)
+{
+	char i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{	
+			if (j > 9)
+			{
+				_putchar(j / 10 + 48);
+			}
+			_putchar(j % 10 + 48);
+		}
+		_putchar('\n');
+	}
+}
